7fcd225bcb2e5d2b95a3fd0832464cdb3038b0df
김태민 <seolin1218@gmail.com>
2023-03-13 22:27:43 +0900

    [fix] : Jenkinsfile publisher 스크립트 로그 저장

--- docker-script.sh
+++ docker-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nohup docker build -t gascharge-app-reservation ~/Users/gimtaemin/k8s/gascharge-app-reservation/ > nohup.out 2>&1 &
+cd ~/Users/gimtaemin/k8s/gascharge-app-reservation/
+nohup docker build -t gascharge-app-reservation . > nohup.out 2>&1 &
