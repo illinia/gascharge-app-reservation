illinia/gascharge-app-batch

[fix] : sshTransfer execCommand 실행시 에러발생해도 무시하는 속성 추가

Jenkinsfile
@@ -47,13 +47,16 @@ pipeline {
                                     sourceFiles: 'application-charge.yml, Dockerfile'
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker stop gascharge-app-reservation-batch'
+                                    execCommand: '/usr/local/bin/docker stop gascharge-app-reservation-batch',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker rm gascharge-app-reservation-batch'
+                                    execCommand: '/usr/local/bin/docker rm gascharge-app-reservation-batch',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker rmi gascharge-app-batch'
+                                    execCommand: '/usr/local/bin/docker rmi gascharge-app-batch',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
                                     execCommand: 'nohup /usr/local/bin/docker build -t gascharge-app-batch k8s/gascharge-app-batch/ > nohup-app-batch-build.out 2>&1 &'

illinia/gascharge-app-reservation

Jenkinsfile
@@ -51,13 +51,16 @@ pipeline {
                                     sourceFiles: 'Dockerfile'
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker stop gascharge-app-reservation-container'
+                                    execCommand: '/usr/local/bin/docker stop gascharge-app-reservation-container',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker rm gascharge-app-reservation-container'
+                                    execCommand: '/usr/local/bin/docker rm gascharge-app-reservation-container',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
-                                    execCommand: '/usr/local/bin/docker rmi gascharge-app-reservation'
+                                    execCommand: '/usr/local/bin/docker rmi gascharge-app-reservation',
+                                    ignoreErrors: true
                                 ),
                                 sshTransfer(
                                     execCommand: 'nohup /usr/local/bin/docker build -t gascharge-app-reservation k8s/gascharge-app-reservation/ > nohup-app-reservation-build.out 2>&1 &'